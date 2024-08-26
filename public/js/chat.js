49bd799c2d75d204c8e57e1e9ababdb09cb29435
codebyanas <m.anaskhalid5@gmail.com>
2024-08-26 14:50:04 +0500

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,80 @@
+const socket = io();
+
+socket.on("message", (message) => {
+    console.log(message);
+});
+
+// Elements
+
+const $messages = document.querySelector('#messages')
+const $messageForm = document.querySelector("#message-form");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messageFormInput = $messageForm.querySelector("#message-input");
+
+// Templates
+const $messageTemplate = document.querySelector('#message-template').innerHTML
+const $locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+socket.on('message', (message) => {
+    console.log(message)
+    const html = Mustache.render($messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render($locationMessageTemplate, {
+        url: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // disable
+    $messageFormButton.setAttribute("disabled", "disabled");
+
+    const message = document.querySelector("#message-input").value;
+
+    socket.emit("sendMessage", message, (error) => {
+        // enabled
+        $messageFormButton.removeAttribute("disabled");
+        $messageFormInput.value = "";
+        $messageFormInput.focus();
+
+        if (error) {
+            console.log(error);
+        }
+
+        console.log("Message is delivered!");
+    });
+});
+
+$sendLocationButton.addEventListener("click", () => {
+    if (!navigator.geolocation) {
+        return alert("Geolocation is not supported by your browser.");
+    }
+
+    // disable
+    $sendLocationButton.setAttribute("disabled", "disabled");
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit(
+            "sendLocation",
+            {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            },
+            () => {
+                $sendLocationButton.removeAttribute("disabled");
+                console.log("Location shared!");
+            }
+        );
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const Filter = require('bad-words')
+const socketio = require('socket.io')
+const { generateMessages } = require('../src/utils/messages')
+const { generateLocationMessage } = require('../src/utils/messages')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectory = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectory))
+
+io.on('connection', (socket) => {
+    console.log('New Websocket connection')
+
+    socket.emit('message', generateMessages('Welcome!'))
+    socket.broadcast.emit('message', generateMessages('A new user joined!'))
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessages(message))
+        callback('Delivered!')
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessages('A new user left!'))
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+
+        console.log('Location shared!')
+        callback()
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
+
+
+
