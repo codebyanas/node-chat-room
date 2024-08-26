73e23c97b09bf89ee73edd9a9e3a51e12b521504
codebyanas <m.anaskhalid5@gmail.com>
2024-08-26 16:14:29 +0500

    Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
-<<<<<<< HEAD
 const Filter = require('bad-words')
 const socketio = require('socket.io')
 const { generateMessages } = require('../src/utils/messages')
 const { generateLocationMessage } = require('../src/utils/messages')
-=======
-const socketio = require('socket.io')
-const Filter = require('bad-words')
->>>>>>> 102172254cb87439c7ebd88227eb4db3706f027b
 
 const app = express()
 const server = http.createServer(app)
@@ -22,16 +17,12 @@ app.use(express.static(publicDirectory))
 
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
-<<<<<<< HEAD
-    console.log('New Websocket connection')
 
     socket.emit('message', generateMessages('Welcome!'))
     socket.broadcast.emit('message', generateMessages('A new user joined!'))
-=======
 
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user joined!')
->>>>>>> 102172254cb87439c7ebd88227eb4db3706f027b
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -40,16 +31,13 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!')
         }
 
-<<<<<<< HEAD
         io.emit('message', generateMessages(message))
-=======
+
         io.emit('message', message)
->>>>>>> 102172254cb87439c7ebd88227eb4db3706f027b
         callback('Delivered!')
     })
 
     socket.on('disconnect', () => {
-<<<<<<< HEAD
         io.emit('message', generateMessages('A new user left!'))
     })
 
@@ -58,7 +46,6 @@ io.on('connection', (socket) => {
 
         console.log('Location shared!')
         callback()
-=======
         io.emit('message', 'A new user left!')
     })
 
@@ -66,7 +53,6 @@ io.on('connection', (socket) => {
         io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
         
         console.log('Location shared!')
->>>>>>> 102172254cb87439c7ebd88227eb4db3706f027b
     })
 })
 
